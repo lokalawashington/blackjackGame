a8d803b373f8b7da0d28623307b0670c381d17c2
peter ngotho <sammykamau772@mail.com>
2021-05-27 08:12:20 -0400

    program working fine

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,6 @@
 import random
+from replit import clear
+from art import logo
 
 
 def deal_cards():
@@ -23,24 +25,66 @@ def calculate_score(cards):
     return sum(cards)
 
 
-# Hint5: deal the user and computer 2 cards each using deal_card
-user_cards = []
-computer_cards = []
+# Hint13
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > computer_score:
+        return "You win 😃"
+    else:
+        return "You lose 😤"
 
-is_game_over = False
 
-for _ in range(2):
-    new_card = deal_cards()
-    user_cards.append(new_card)
-    computer_cards.append(new_card)
-# Hint9: call calculate_score(). if the computer or user has a blackjack (0) of if the user's scores is over 21,
-# then the game ends
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
-print(f"Your card : {user_cards}, current score: {user_score} ")
-print(f"Computer first card : {computer_cards[0]}")
+def play_game():
+    print(logo)
+    # Hint5: deal the user and computer 2 cards each using deal_card
+    user_cards = []
+    computer_cards = []
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    is_game_over = False
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
+    for _ in range(2):
+        new_card = deal_cards()
+        user_cards.append(new_card)
+        computer_cards.append(new_card)
+    # # Hint 11
+    while not is_game_over:
+        # Hint9: call calculate_score(). if the computer or user has a blackjack (0) of if the user's scores is over 21,
+        # then the game ends
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your card : {user_cards}, current score: {user_score} ")
+        print(f"Computer first card : {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_cards())
+            else:
+                is_game_over = True
+
+    # hint 12
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_cards())
+        computer_score = calculate_score(computer_cards)
+
+    print(f" Your final hand: {user_cards},final score {user_score} ")
+    print(f" Your final hand: {computer_cards},final score {computer_score} ")
+    print(compare(user_score, computer_score))
+
+
+# hint 14
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    clear()
+    play_game()
+
